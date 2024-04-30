e2881fc11fb657feea6d1e8e882a1e2845798cce
Auoggi <75486866+Auoggi@users.noreply.github.com>
2024-04-30 12:40:31 +0200

    Remove comment from player

--- Player.py
+++ Player.py
@@ -8,7 +8,7 @@ class PlayerClass:
     color=(0, 128, 255)
     points=0
 
-    def __init__(self,screen,xpos,ypos):#,terrainCollection):
+    def __init__(self,screen,xpos,ypos):
         self.x=xpos
         self.y=ypos
         self.theScreen=screen
