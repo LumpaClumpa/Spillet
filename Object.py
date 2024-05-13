7149d0a0b0a377e1b5a0f96d07531e2a7a1209a5
shitman <h.lykkeskov@gmail.com>
2024-05-13 12:25:05 +0200

    ændrer button til et billede

--- Object.py
+++ Object.py
@@ -2,19 +2,12 @@ import pygame
 
 # TODO rewrite to support rendering sprites instead of colours
 class Object:
-    def __init__(self, screen : pygame.surface.SurfaceType, x, y, color, radiusOrWidth, height=0):
-        self.screen = screen
-        self.x = x + (screen.get_width()/2 - 25) # 25 is half of player width
-        self.y = y + (screen.get_height()/2 - 25) # 25 is half of player height
-        self.color = color
-        if height == 0:
-            self.radius = radiusOrWidth
-        else:
-            self.width = radiusOrWidth
-            self.height = height
+    def __init__(self, screenHeight, screenWidth, display, x, y, image, size):
+        self.display = display
+        self.x = x + (screenWidth/2 - 25) # 25 is half of player width
+        self.y = y + (screenHeight/2 - 25) # 25 is half of player height
+        self.image = image
+        self.size = size
 
     def draw(self):
-        if hasattr(self, 'radius'):
-            pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
-        else:
-            pygame.draw.rect(self.screen, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
+        self.display.blit(self.image, (self.x, self.y))
