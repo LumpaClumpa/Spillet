6f23b4029b6717c25b861c6aa00ad952494b29da
auoggi <augustoggiahrens@gmail.com>
2024-05-16 09:19:14 +0200

    Remove unnecessary import and data type

--- Player.py
+++ Player.py
@@ -1,8 +1,8 @@
-import pygame.surface
+import pygame
 from Object import Object
 
 class Player(Object):
-    def __init__(self, display : pygame.Surface):
+    def __init__(self, display):
         super().__init__(display.get_width(), display.get_height(), display, 25, 25, pygame.image.load('assets/player.png'))
 
     def draw(self):
