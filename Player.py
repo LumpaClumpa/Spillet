bc8a1a1e8d81e3594f59c1ad963cc9251a7a93fb
auoggi <augustoggiahrens@gmail.com>
2024-05-23 09:06:47 +0200

    Lav plæne til 10x10 pixel grid, mangler at opdatere coin function

--- Player.py
+++ Player.py
@@ -3,7 +3,8 @@ from Object import Object
 
 class Player(Object):
     def __init__(self, display):
-        super().__init__(display, 25, 25, pygame.image.load('assets/player.png'))
+        super().__init__(display, 2, 2, pygame.image.load('assets/player.png'))
 
     def draw(self):
         super().draw(0, 0)
+        #self.display.blit(pygame.transform.scale(self.image, (40, 40)), (self.x + (self.display.get_width()/2 - 2), self.y + (self.display.get_height()/2 - 2)))
