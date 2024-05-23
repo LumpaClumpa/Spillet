0429bf66ff3397d9578290f5591b5f87be57de89
shitman <h.lykkeskov@gmail.com>
2024-05-23 10:44:16 +0200

    giv dunktion til grass upgrade

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ display = pygame.display.set_mode((gameWindowWidth, gameWindowHeight))
 button_surface = pygame.Surface((120, 92))
 background = pygame.Surface((1920, 1080))
 
-lawnSize = (120, 120)
+lawnSize = (50, 50)
 lawn = pygame.Surface(lawnSize)
 lawn.fill((65, 152, 10))
 
@@ -35,8 +35,8 @@ def spawnCoin(amount=1):
 def removeCoin(i):
     coins.pop(i)
 
-
-spawnCoin(math.ceil(lawnSize[0] * lawnSize[1] / 1000))
+maxCoins = math.ceil(lawnSize[0] * lawnSize[1] / 1000)
+spawnCoin(maxCoins)
 
 imageIdleShop = pygame.transform.scale(pygame.image.load('assets/shop_icon1.png'), (70, 60))
 imageHoverShop = pygame.transform.scale(pygame.image.load('assets/shop_icon2.png'), (70, 60))
@@ -51,6 +51,9 @@ shop_open = False
 def draw_shop(surface):
     surface.blit(imageShopmenu, (display.get_width()/2 - imageShopmenu.get_width()/2, display.get_height()/2 - imageShopmenu.get_height()/2))
     lawnupgrade.draw()
+    lawnupgradePrice = font.render('Price: ' + str(maxCoins), False, (255, 255, 255))
+    display.blit(lawnupgradePrice, (lawnupgrade.x + 10, lawnupgrade.y + 80))
+
 
 speed, x, y = 1, 0, 0
 
@@ -67,8 +70,15 @@ while not done:
             if shopbutton.isOver(pygame.mouse.get_pos()):
                 shop_open = not shop_open
 
-            if lawnupgrade.isOver(pygame.mouse.get_pos()) and shop_open:
-                lawnSize = (25, 25) #this is a test
+            if lawnupgrade.isOver(pygame.mouse.get_pos()) and shop_open and coinCount >= maxCoins:
+                coinCount -= maxCoins
+                lawn.fill((65, 152, 10))
+                x = 0
+                y = 0
+                lawnSize = (lawnSize[0] + 50, lawnSize[1] + 50) #temp
+                maxCoins = math.ceil(lawnSize[0] * lawnSize[1] / 1000)
+                spawnCoin(maxCoins)
+
 
     if frameCount % 2 == 0 and not shop_open:
         up = pygame.key.get_pressed()[pygame.K_UP] or pygame.key.get_pressed()[pygame.K_w]
